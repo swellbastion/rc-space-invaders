df1140d2c077cf685b9ee7dc5763e5cd5e854700
Jesse Olsson <swellbastion@gmail.com>
2026-06-03 13:24:15 -0400

    let aliens move down when they hit a wall

--- javascript/Alien.js
+++ javascript/Alien.js
@@ -6,6 +6,8 @@ export class Alien {
         this.height = height;
         this.xDirection = xDirection;
         this.speed = speed;
+
+        this.desiredYPosition = y; // aliens will gradually move down towards their desired y position;
     }
 
     isHittingLeftEdge = (levelWidth) => {
@@ -20,11 +22,14 @@ export class Alien {
         if (this.isHittingLeftEdge(levelWidth)) {
             this.x = 0;
             this.xDirection = 1;
+            this.desiredYPosition += this.height;
         }
         else if (this.isHittingRightEdge(levelWidth)) {
             this.x - levelWidth - this.width;
             this.xDirection = -1;
+            this.desiredYPosition += this.height;
         }
         this.x += this.speed * this.xDirection * timeDelta;
+        if (this.desiredYPosition > this.y) this.y += this.speed * timeDelta;
     };
 };
